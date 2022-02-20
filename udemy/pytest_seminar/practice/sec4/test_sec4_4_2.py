e4bdb72df1899f49b9caa481f60e58cde8107f85
Keido Sumi <delta0726@nta.pial.jp>
2022-02-21 06:49:37 +0900

    Update pytest sec4-4-1

--- udemy/pytest_seminar/practice/sec4/test_sec4_4_2.py
+++ udemy/pytest_seminar/practice/sec4/test_sec4_4_2.py
@@ -1,7 +1,7 @@
 # ******************************************************************************
 # Course      : 0から始めるPytest超基礎講座
 # Chapter     : 4 fixtureによる前処理＆後処理の代替
-# Theme       : fixtureによるモジュールの前処理/後処理の代替（モジュールのみ）
+# Theme       : fixtureによるモジュールの前処理/後処理の代替（モジュール + 関数）
 # Creat Date  : 2022/2/20
 # Final Update:
 # URL         : https://www.udemy.com/course/python_pytest/
@@ -9,26 +9,25 @@
 
 
 # ＜ポイント＞
-# - 今回(sec4-4-1)はモジュール(関数とクラス)に対して前処理/後処理を行う
-#   --- 前回(sec4-2)ではクラスの各メソッドに対して前処理/後処理を行った
-#   --- デコレータ(@pytest.fixture)でスコープ引数で"scope=module"を設定する
+# - モジュール全体に対して前処理/後処理をしつつ、特定の関数でも前処理/後処理を行うことができる
+#   --- 関数の引数にそれぞれの前処理/後処理の指定する
 
 
 # ＜実行コマンド＞
 # - ターミナルより実行
-# pytest I:/Project/Python/py_practice/udemy/pytest_seminar/practice/sec4/test_sec4_4_1.py -s
+# pytest I:/Project/Python/py_practice/udemy/pytest_seminar/practice/sec4/test_sec4_4_2.py -s
 
 
 # ＜出力結果＞
-# - モジュール(関数とクラス)をはさんで前処理/後処理を実行している
+# - モジュール全体に前処理/後処理を実行しつつ、特定の関数でも独自の前処理/後処理を実行する
 # setup_module
-# function: hello world!
+# setup_function
+# function: hello world! (*)
+# teardown_function
 # function: pytest
-# method: hello world!
-# method: pytest
+# class: hello world!
+# class: pytest
 # teardown_module
-
-
 
 
 # メイン処理 --------------------------------------------------------------
@@ -46,8 +45,19 @@ def setup_module(request):
         print("teardown_module")
     request.addfinalizer(teardown_module)
 
+# 関数定義
+# --- 前処理/後処理
+# --- scopeはフィクスチャ関数が実行される粒度を指定（"function"を指定）
+@pytest.fixture(scope="function")
+def setup_function(request):
+    print("setup_function")
+    def teardown_function():
+        print("teardown_function")
+    request.addfinalizer(teardown_function)
+
 # 関数定義1：
-def test_hello_world(setup_module):
+# --- モジュールのテストと関数のテストの両方で前処理/後処理を行う
+def test_hello_world(setup_module, setup_function):
     print("function: hello world!")
 
 # 関数定義2：
@@ -57,7 +67,7 @@ def test_pytest(setup_module):
 # クラス定義
 class TestExample():
     def test_hello_world(self, setup_module):
-        print("method: hello world!")
+        print("class: hello world!")
 
     def test_pytest(self, setup_module):
-        print("method: pytest")
+        print("class: pytest")
