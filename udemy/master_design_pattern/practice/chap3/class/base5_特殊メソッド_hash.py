ce6d62d03bdd93a89e9db35bcb03207097493d9d
Keido Sumi <delta0726@nta.pial.jp>
2022-03-02 20:13:43 +0900

    Update chap3-class-base4 __hash__

--- udemy/master_design_pattern/practice/chap3/class/base5_特殊メソッド_hash.py
+++ udemy/master_design_pattern/practice/chap3/class/base5_特殊メソッド_hash.py
@@ -3,14 +3,14 @@
 # Chapter     : 3 Python基本文法講座
 # Theme       : 60 特殊メソッド2  __hash__
 # Creat Date  : 2022/2/6
-# Final Update:
+# Final Update: 2022/3/2
 # URL         : https://www.udemy.com/course/python-mx/
 # ******************************************************************************
 
 
 # ＜概要＞
-# - 特殊メソッドはクラスに定型的な機能を持たせるために実装する
-# - __hash__は文字列のハッシュ変換を行う
+# - 特殊メソッドはクラスに定型的な機能を持たせるために実装するメソッド
+# - __hash__は文字列のハッシュ変換(数値変換)を行う
 
 
 # ＜目次＞
@@ -59,20 +59,31 @@ class Human:
 
 # 3 __hash__を実行する ----------------------------------------------
 
+# ＜ポイント＞
+# - __hash__はsetなどオブジェクトに一意性が求められる際に適用される
+
+
 # インスタンス生成
 # --- man1とman2は__eq__で同一と判断される
 man1 = Human(name='Taro', age=20, phone_number='08011111111')
 man2 = Human(name='Taro', age=18, phone_number='08011111111')
 man3 = Human(name='Jiro', age=18, phone_number='09011111111')
 
+# 出力確認
+print(man1)
+print(man2)
+print(man3)
+
 # 一致確認
+# --- nameとphone_numberで一致確認
 print(man1 == man2)
 print(man1 == man3)
 print(man2 == man3)
 
 # インスタンスをセットに格納
-# --- 同じものは格納できない
+# --- 同じものは格納できないのでhash変換が行われる
 set_men = {man1, man2, man3}
+print(set_men)
 
 # 確認
 # --- 要素が2つしか格納されていない（man1とman2はイコールなため）
